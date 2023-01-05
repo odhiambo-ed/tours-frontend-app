715e27026ebf75cfa423d565662dd87660754b6c
white3d <odhiambo.edward111@gmail.com>
2023-01-05 20:25:04 +0300

    Add Tour Component

---
+++ src/Tour.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Tour = ({ id, image, info, name, price }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <button className="delete-btn">
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
