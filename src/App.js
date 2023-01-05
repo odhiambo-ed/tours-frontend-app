4f85305efc9e632dad0a4b9c4b5d2e5b28baf2c0
white3d <odhiambo.edward111@gmail.com>
2023-01-05 20:36:39 +0300

    Add Fetch Function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const fetchTours = async () => { 
+    setLoading(true);
+
+    try { 
+      const response = await fetch(url)
+      const tours = response.json()
+      setLoading(false);
+      setTours(tours);
+
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+   )
+  } else {
+    return (
+      <main>
+        <Tours tours={tours} />
+      </main>
+    )
+  }
 }
 
-export default App;
+export default App
